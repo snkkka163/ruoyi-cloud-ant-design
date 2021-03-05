563ebe67cafa82dfff59a8ff3212b17ebddb90b6
2531639937@qq.com <2531639937@qq.com>
2021-03-05 10:57:29 +0800

    update hint message

--- src/views/user/Login.vue
+++ src/views/user/Login.vue
@@ -83,12 +83,12 @@
       <!-- </a-tabs> -->
 
       <a-form-item>
-        <a-checkbox v-decorator="['rememberMe', { valuePropName: 'checked' }]">自动登录</a-checkbox>
-        <router-link
+        <a-checkbox v-decorator="['rememberMe', { valuePropName: 'checked' }]">记住密码</a-checkbox>
+        <!-- <router-link
           :to="{ name: 'recover', params: { user: 'aaa'} }"
           class="forge-password"
           style="float: right;"
-        >忘记密码</router-link>
+        >忘记密码</router-link> -->
       </a-form-item>
 
       <a-form-item style="margin-top:24px">
